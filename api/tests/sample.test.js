0d6a237756f8a4a4b08c764f8b728b9e3ef402aa
Beesama <gbozee@gmail.com>
2019-01-29 18:34:01 +0100

    completed the implementation with test scenarios for creating a political party

--- api/tests/sample.test.js
+++ api/tests/sample.test.js
@@ -1,7 +1,61 @@
 const { expect } = require('chai');
+const request = require('supertest');
+const app = require('../src/server');
 
-describe('Sample Test', () => {
-  it('Should work correctly', () => {
-    expect(1 + 1).to.equal(2);
+describe('POST /party', () => {
+  const url = '/api/v1/parties';
+  it('when a party is successfully created', () => {
+    const partyDetails = {
+      name: 'PDP',
+      hqAddress: 'Abuja',
+      logoUrl: 'http://www.google.com',
+    };
+    request(app)
+      .post(url)
+      .send(partyDetails)
+      .end((err, res) => {
+        expect(res.statusCode).to.equal(201);
+        const { body } = res;
+        expect(body).to.deep.equal({
+          status: 201,
+          data: {
+            id: 1,
+            name: 'PDP',
+            hqAddress: 'Abuja',
+            logoUrl: 'http://www.google.com',
+          },
+        });
+      });
+  });
+  it('When a second party is added', () => {
+    request(app)
+      .post(url)
+      .send({ name: 'APC', hqAddress: 'Lagos', logoUrl: 'http://facebook.com' })
+      .end((err, res) => {
+        expect(res.statusCode).to.equal(201);
+        const { body } = res;
+        expect(body).to.deep.equal({
+          status: 201,
+          data: {
+            id: 2,
+            name: 'APC',
+            hqAddress: 'Lagos',
+            logoUrl: 'http://facebook.com',
+          },
+        });
+      });
+  });
+  it("when a party wasn't successfully created", () => {
+    const partyDetail = {
+      hqAddress: 'Abuja',
+      logoUrl: 'http://www.google.com',
+    };
+    request(app).post(url).send(partyDetail).end((err, res) => {
+      expect(res.statusCode).to.equal(400);
+      expect(res.body).to.deep.equal({
+        status: 400,
+        data: 'The name of the party is missing',
+      });
+    });
   });
 });

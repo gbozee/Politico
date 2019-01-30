25d873ca42f13cb8e3064442f11e2ecb36714f58
Beesama <gbozee@gmail.com>
2019-01-30 20:40:40 +0100

    office resource creation endpoint

--- api/src/server.js
+++ api/src/server.js
@@ -11,13 +11,15 @@ app.get('/', (req, res) => {
 const version = 'v1';
 
 let parties = [];
-const requiredFields = ['name'];
+const offices = [];
+const partyRequiredFields = ['name'];
+const officesRequiredFields = ['name', 'type'];
 const baseUrl = `/api/${version}`;
 app.post(`${baseUrl}/parties`, (req, res) => {
   const { body } = req;
   let isValid = true;
   const keys = Object.keys(body);
-  requiredFields.forEach((field) => {
+  partyRequiredFields.forEach((field) => {
     isValid = isValid && keys.includes(field);
   });
   if (isValid) {
@@ -35,7 +37,7 @@ app.post(`${baseUrl}/parties`, (req, res) => {
       data,
     });
   } else {
-    const missingKeys = requiredFields.filter(
+    const missingKeys = partyRequiredFields.filter(
       key => !Object.keys(body).includes(key),
     );
     res.status(400).json({
@@ -93,6 +95,38 @@ app.delete(`${baseUrl}/parties/:partyId`, (req, res) => {
     });
   }
 });
+
+app.post(`${baseUrl}/offices`, (req, res) => {
+  const { body } = req;
+  let isValid = true;
+  const keys = Object.keys(body);
+  officesRequiredFields.forEach((field) => {
+    isValid = isValid && keys.includes(field);
+  });
+  if (isValid) {
+    const id = offices.length + 1;
+    const data = {
+      id,
+      name: body.name,
+      type: body.type,
+    };
+    offices.push(data);
+    const status = 201;
+    res.status(status).json({
+      status,
+      data,
+    });
+  } else {
+    const missingKeys = officesRequiredFields.filter(
+      key => !Object.keys(body).includes(key),
+    );
+    res.status(400).json({
+      status: 400,
+      error: `The ${missingKeys[0]} of the office is missing`,
+    });
+  }
+});
+
 
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
